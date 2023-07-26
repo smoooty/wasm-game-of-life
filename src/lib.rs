032a904ff8c63f060b8ead299209b772edd42b7e
Spencer Pope <spenceropope@gmail.com>
2023-07-25 20:41:44 -0400

    Refactor to use fixedbitset so that each cell is only a bit instead of a byte

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,13 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-use std::fmt;
-
-#[wasm_bindgen]
-#[repr(u8)]
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum Cell {
-    Dead = 0,
-    Alive = 1,
-}
+use fixedbitset::FixedBitSet;
 
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 
 /// Public methods, exported to JavaScript.
@@ -31,24 +23,21 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
+                next.set(
+                    idx,
+                    match (cell, live_neighbors) {
+                        // Rule 1: Any live cell with fewer than two live neighbours
+                        (true, x) if x < 2 => false,
+                        // Rule 2: Any live cell with two or three live neighbours
+                        (true, 2) | (true, 3) => true,
+                        // Rule 3: Any live cell with more than three live
+                        (true, x) if x > 3 => false,
+                        // Rule 4: Any dead cell with exactly three live neighbours
+                        (false, 3) => true,
+                        // All other cells remain in the same state.
+                        (otherwise, _) => otherwise,
+                    },
+                );
             }
         }
 
@@ -80,15 +69,12 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0)
+        }
 
         Universe {
             width,
@@ -97,27 +83,21 @@ impl Universe {
         }
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 }
 
 impl Default for Universe {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            writeln!(f)?;
-        }
-
-        Ok(())
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,18 +1,104 @@
 // @ts-check
 // I just set, "js/ts.implicitProjectConfig.checkJs": true in vscode settings
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
 import { Universe } from "wasm-game-of-life";
 
-const pre = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
 
+// Construct the universe, and get its width and height.
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
 
-const renderLoop = () => {
-  if (pre) {
-    pre.textContent = universe.render();
-  }
-  universe.tick();
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
 
+const canvas = document.getElementById("game-of-life-canvas");
+if (canvas instanceof HTMLCanvasElement) {
+  canvas.height = (CELL_SIZE + 1) * height + 1;
+  canvas.width = (CELL_SIZE + 1) * width + 1;
+
+  const ctx = canvas.getContext("2d");
+
+  const renderLoop = () => {
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(renderLoop);
+  };
+
+  const drawGrid = () => {
+    if (ctx instanceof CanvasRenderingContext2D) {
+      ctx.beginPath();
+      ctx.strokeStyle = GRID_COLOR;
+
+      // Vertical lines.
+      for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+      }
+
+      // Horizontal lines.
+      for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+      }
+
+      ctx.stroke();
+    }
+  };
+
+  const getIndex = (row, column) => {
+    return row * width + column;
+  };
+
+  const bitIsSet = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << n % 8;
+    return (arr[byte] & mask) === mask;
+  };
+
+  const drawCells = () => {
+    if (ctx instanceof CanvasRenderingContext2D) {
+      const cellsPtr = universe.cells();
+
+      // This is updated!
+      const cells = new Uint8Array(
+        memory.buffer,
+        cellsPtr,
+        (width * height) / 8
+      );
+
+      ctx.beginPath();
+
+      for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+          const idx = getIndex(row, col);
+
+          // This is updated!
+          // if the bit is set (enabled) then it is alive
+          ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
+
+          ctx.fillRect(
+            col * (CELL_SIZE + 1) + 1,
+            row * (CELL_SIZE + 1) + 1,
+            CELL_SIZE,
+            CELL_SIZE
+          );
+        }
+      }
+
+      ctx.stroke();
+    }
+  };
+
+  drawGrid();
+  drawCells();
   requestAnimationFrame(renderLoop);
-};
-
-requestAnimationFrame(renderLoop);
+}
